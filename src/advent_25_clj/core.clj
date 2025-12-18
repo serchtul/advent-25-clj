380815d93c80ede5f3eed0da2b08ff769ee34bb2
Sergio García <3311638+serchtul@users.noreply.github.com>
2025-12-17 21:01:27 -0600

    Day 1 - full solution

--- src/advent_25_clj/core.clj
+++ src/advent_25_clj/core.clj
@@ -4,16 +4,30 @@
 
 (defn parse-reading [reading]
   (let [sign (if (= (first reading) \L) -1 1)]
+;    (print reading "")
     (* sign (parse-long (subs reading 1)))))
 
-(defn rotate-dial [{curr-count :count, zeros :zeros} value]
-  (let [next-count (mod (+ curr-count value) 100)]
-    {:count (if (neg? next-count) (+ next-count 100) next-count)
-     :zeros (if (zero? next-count) (inc zeros) zeros)}))
+(defn rotate-dial [new-zeros-fn {curr-count :count, zeros :zeros} value]
+  (let [next-count (mod (+ curr-count value) 100)
+        new-zeros (new-zeros-fn next-count curr-count value)]
+;    (println "next" next-count "new zeros" new-zeros)
+    {:count next-count :zeros (+ zeros new-zeros)}))
+
+(defn solve [result-str new-zeros-fn]
+  (with-open [reader (io/reader "input_day1.txt")]
+    (println result-str
+             (reduce
+              (partial rotate-dial new-zeros-fn)
+              {:count 50 :zeros 0}
+              (map parse-reading (line-seq reader))))))
 
 (defn -main
   []
-  (with-open [reader (io/reader "input_day1.txt")]
-    (println "Result:"
-             (reduce rotate-dial {:count 50 :zeros 0} (map parse-reading (line-seq reader))))))
-
+  (solve "Part 1 Result:" (fn [next-count & _] (if (zero? next-count) 1 0)))
+  (solve "Part 2 Result:" (fn [next-count curr-count value]
+                            (let [full-turns (int (/ (abs value) 100))
+                                  not-zero-counts? (not (or (zero? next-count) (zero? curr-count)))
+                                  clicked-right? (and (pos? value) (< next-count curr-count) not-zero-counts?)
+                                  clicked-left? (and (neg? value) (> next-count curr-count) not-zero-counts?)
+                                  count-zero? (and (zero? next-count) (pos? (mod value 100)))]
+                              (+ full-turns (count (filter true? [count-zero? clicked-right? clicked-left?])))))))
