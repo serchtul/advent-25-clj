00adfb13fd5b24848b2b09020e7a69a77155ff42
Sergio García <3311638+serchtul@users.noreply.github.com>
2026-05-30 19:29:06 -0600

    day 3 solution

--- src/advent_25_clj/day_3.clj
+++ src/advent_25_clj/day_3.clj
@@ -9,19 +9,27 @@
                   {:n -1 :i -1})
        (#(update % :n - (int \0)))))
 
-(defn max-joltage [bank]
-  (let [max-tens (-> bank drop-last find-max-indexed)
-        max-units (->> bank
-                       (drop (+ 1 (:i max-tens)))
-                       find-max-indexed)]
-    (parse-long (str (:n max-tens) (:n max-units)))))
+(defn max-joltage [num-batteries bank]
+  (loop [curr-bank bank
+         idx       1
+         result    []]
+    (let [{:keys [n i]} (->> curr-bank
+                             (drop-last (- num-batteries idx))
+                             find-max-indexed)
+          new-result    (conj result n)]
+      (if (< idx num-batteries)
+        (recur (drop (+ i 1) curr-bank)
+               (inc idx)
+               new-result)
+        (->> new-result (apply str) parse-long)))))
 
-(defn solve []
+(defn solve [num-batteries]
   (with-open [input (io/reader "input_day3.txt")]
     (->> input
          line-seq
-         (map max-joltage)
+         (map #(max-joltage num-batteries %))
          (reduce +))))
 
 (defn -main []
-  (println "Part 1:" (solve)))
+  (println "Part 1:" (solve 2))
+  (println "Part 2:" (solve 12)))
