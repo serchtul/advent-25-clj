feb6cc1fd2db32cf8fdaffb5a38f5190b5d9b010
Sergio García <3311638+serchtul@users.noreply.github.com>
2026-06-02 02:35:54 -0600

    day 5 solution

---
+++ src/advent_25_clj/day_5.clj
@@ -0,0 +1,55 @@
+(ns advent-25-clj.day-5
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as s]))
+
+(defn parse-range [line]
+  (let [[lower upper] (s/split line #"-")]
+    {:lower (parse-long lower)
+     :upper (parse-long upper)}))
+
+(defn fresh-item? [fresh-coll item]
+  (some #(<= (:lower %) item (:upper %)) fresh-coll))
+
+(defn parse-input []
+  (with-open [input (io/reader "input_day5.txt")]
+    (let [[fresh-lines available-lines] (->> input
+                                             line-seq
+                                             doall
+                                             (split-with #(not= % "")))
+          fresh     (->> fresh-lines
+                         (map parse-range)
+                         (sort-by (juxt :lower :upper)))
+          available (->> available-lines
+                         rest
+                         (map parse-long))]
+      {:fresh fresh
+       :available available})))
+
+(defn solve-part1 []
+  (let [{:keys [fresh available]} (parse-input)]
+    (->> available
+         (filter #(fresh-item? fresh %))
+         count)))
+
+(defn remove-overlaps [fresh]
+  (fn [idx item]
+    (let [prev-upper   (:upper (nth fresh (- idx 1) nil))
+          overlapping? (some-> prev-upper (>= (:lower item)))]
+      (if overlapping?
+        (let [lower (+ prev-upper 1)
+              upper (:upper item)]
+          (when (<= lower upper) {:lower lower :upper upper}))
+        item))))
+
+(defn solve-part2 []
+  (let [{:keys [fresh]} (parse-input)]
+    (->> fresh
+         (#(map-indexed (remove-overlaps %) %))
+         (filter some?)
+         (map #(- (:upper %) (:lower %) -1))
+         (reduce +))))
+
+(defn -main []
+  (println "Part 1:" (solve-part1))
+  (println "Part 2:" (solve-part2)))
