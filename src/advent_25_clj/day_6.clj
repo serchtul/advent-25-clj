1c69dd1423cb6d12e2c29961e1bb303eb52b09d9
Sergio García <3311638+serchtul@users.noreply.github.com>
2026-06-04 20:18:37 -0600

    day 6 solution

--- src/advent_25_clj/day_6.clj
+++ src/advent_25_clj/day_6.clj
@@ -7,7 +7,7 @@
     (or (parse-long clean)
         (-> clean symbol resolve))))
 
-(defn solve []
+(defn solve-part1 []
   (->> (slurp "input_day6.txt")
        (s/split-lines)
        (map s/trim)
@@ -17,5 +17,29 @@
        (apply map #(apply %1 %&))
        (reduce +)))
 
+(defn parse2 [item]
+  (if (contains? #{\+ \*} (last item))
+    [(-> item
+         (#(subs % 0 (dec (count %))))
+         s/trim
+         parse-long)
+     (-> item last str symbol resolve)]
+    (parse-long (s/trim item))))
+
+(defn solve-part2 []
+  (->> (slurp "input_day6.txt")
+       (s/split-lines)
+       (map s/reverse)
+       (map #(s/split % #""))
+       (apply map str)
+       (map parse2)
+       (filter some?)
+       flatten
+       (partition-by number?)
+       (partition 2)
+       (map (fn [[args [f]]] (apply f args)))
+       (reduce +)))
+
 (defn -main []
-  (println "Part 1:" (solve)))
+  (println "Part 1:" (solve-part1))
+  (println "Part 2:" (solve-part2)))
